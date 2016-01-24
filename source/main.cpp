5a837d231c81076ff607c6d9bc0e494bea4b25db
Avinaba Dalal <d97.avinaba@gmail.com>
2016-01-24 22:13:29 +0530

    main: Move to init fn

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <ui.h>
+#include <log.h>
+
+#define LOG_TAG "Main"
+
+int check_for_qfile() {
+	FILE *f;
+	f = fopen("qfile.bin", "rb");
+	if(!f)
+		return 1;
+	fclose(f);
+	return 0;
+}
+
+void init() {
+	logi(LOG_TAG,"Deleting old log file!");
+	if(remove("log.txt"))
+		loge(LOG_TAG,"No log file! First start?");
+	else
+		logi(LOG_TAG,"Succesfully deleted log file");
+	logi(LOG_TAG,"Welcome to QUIZ");
+	logi(LOG_TAG,"Starting up init process!");
+	logi(LOG_TAG,"Checking for qfile!");
+	if (check_for_qfile()) {
+			ui_error();
+			loge(LOG_TAG,"qfile not found! Exiting");
+			exit(0);
+	} else 
+		logi(LOG_TAG,"qfile found!");
+	logi(LOG_TAG,"Starting up UI");
+	ui_main();
+	
+}
 
 int main() {
-	ui_main();
+	init();
 	return 0;
 }
