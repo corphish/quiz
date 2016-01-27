c484a7497807bf2763db3ec6c2211d5db566cf11
Avinaba Dalal <d97.avinaba@gmail.com>
2016-01-27 20:32:31 +0530

    log: Fix header definitions

--- include/log.h
+++ include/log.h
@@ -1,3 +1,3 @@
-void logv(char* log_tag, char* message);
-void loge(char* log_tag, char* message);
-void logi(char* log_tag, char* message);
+void logv(char log_tag[], char message[]);
+void loge(char log_tag[], char message[]);
+void logi(char log_tag[], char message[]);
